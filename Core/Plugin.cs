d6c7565ec0d9d8ae528cca6ffe41d17297c93dee
fiote <murilo.eng@gmail.com>
2024-02-09 22:34:11 -0300

    xxx - moving patch to player.usehotbaritem so it happens before usefultrophies, moving offer logic to main file

--- Core/Plugin.cs
+++ Core/Plugin.cs
@@ -39,6 +39,8 @@ namespace BossDirections {
 			Bar();
 		}
 
+		#region SETUP
+
 		void LoadConfig() {
 			Line();
 			Debug("LoadConfig()");
@@ -62,7 +64,83 @@ namespace BossDirections {
 			if (offerings.Count == 0) {
 				Error("No offerings loaded. Are you sure you have BossDirections/offerings.json on your plugins folder?");
 			}
-		}	
+		}
+
+
+
+		#endregion
+		#region METHODS
+		public static bool TryOffer(ItemDrop.ItemData item) {
+			var iname = item.m_shared.m_name.ToLower();
+			Log($"Fireplace_UseItem_Patch Prefix {iname} stack={item.m_stack}");
+
+			var neededAmount = 0;
+			var bossLocation = "";
+			var bossName = "";
+			var addname = false;
+			var talks = new List<string>();
+
+			// checking all offerings
+			offerings.ForEach(offering => {
+				// checking items for each offering
+				foreach (var offitem in offering.items) {
+					// checking if the item is right
+					if ("$item_" + offitem.Key.ToLower() == iname) {
+						// setting all the values needed for this to work
+						neededAmount = offitem.Value;
+						bossLocation = offering.location;
+						bossName = offering.name;
+						talks = offering.quotes;
+						addname = offering.addname;
+					}
+				}
+			});
+
+			Log($"neededAmount={neededAmount} bossCode={bossLocation} bossName={bossName} talks={talks.Count}");
+
+			// if we did not find a location, stop here
+			if (bossLocation == "") return false;
+
+			var player = Player.m_localPlayer;
+
+			// if the player doesnt have the needed amount
+			if (item.m_stack < neededAmount) {
+				// let they know
+				player.Message(MessageHud.MessageType.Center, "$msg_toofew " + iname);
+				// return true because this was an offering item
+				return true;
+			}
+
+			// getting a random boss quote
+			var rnd = new System.Random();
+			var randomIndex = rnd.Next(talks.Count);
+			var bossTalk = talks[randomIndex];
+			if (addname) bossTalk = $"[{bossName}]: " + bossTalk;
+
+			// removing the item from the player's inventory
+			player.GetInventory().RemoveItem(item.m_shared.m_name, neededAmount);
+
+			// showing the boss quote
+			player.Message(MessageHud.MessageType.Center, bossTalk);
+
+			// getting the player location
+			var point = player.gameObject.transform.position;
+
+			if (config.pinless) {
+				Log("Discovering location in Pinless mode...");
+				ZoneSystem.LocationInstance closest;
+				ZoneSystem.instance.FindClosestLocation(bossLocation, point, out closest);
+				Player.m_localPlayer.SetLookDir(closest.m_position - Player.m_localPlayer.transform.position, 3.5f);
+			} else {
+				Log("Discovering location in normal mode...");
+				Game.instance.DiscoverClosestLocation(bossLocation, point, bossName, (int)Minimap.PinType.Boss, false, false);
+			}
+
+			Log("Offering done!");
+			return true;
+		}
+
+		#endregion
 
 		#region LOGGING
 
---
+++ Patches/Player_Patch.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+namespace BossDirections.Patches {
+
+	[HarmonyPatch(typeof(Player), "UseHotbarItem")]
+
+	public class Player_UseHotbarItem_Patch {
+		[HarmonyPrefix]
+		static bool Prefix(Player __instance, int index) {
+			ItemDrop.ItemData item = __instance.GetInventory().GetItemAt(index - 1, 0);
+			var offered = BossDirections.TryOffer(item);
+			return !offered;
+		}
+	}
+
+}
