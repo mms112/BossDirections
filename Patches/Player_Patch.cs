b895082d3cba3c674e9b16a52101a8ac17e571b2
fiote <murilo.eng@gmail.com>
2024-02-10 11:30:53 -0300

    xxx - checking for fireplaces before offering

--- Patches/Player_Patch.cs
+++ Patches/Player_Patch.cs
@@ -6,7 +6,16 @@ namespace BossDirections.Patches {
 	public class Player_UseHotbarItem_Patch {
 		[HarmonyPrefix]
 		static bool Prefix(Player __instance, int index) {
-			ItemDrop.ItemData item = __instance.GetInventory().GetItemAt(index - 1, 0);
+			var item = __instance.GetInventory().GetItemAt(index - 1, 0);
+			if (item == null) return false;
+
+			var hoverObject = __instance.GetHoverObject();
+			if (hoverObject == null) return false;
+			
+			var fireplaceObject = hoverObject.GetComponent<Fireplace>();
+			var fireplaceParent = hoverObject.GetComponentInParent<Fireplace>();
+			if (fireplaceObject == null && fireplaceParent == null) return false;
+
 			var offered = BossDirections.TryOffer(item);
 			return !offered;
 		}
